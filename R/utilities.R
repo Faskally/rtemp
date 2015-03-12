5566692074408fab6aee108aaf6b25c45898e24c
colinpmillar <millarc@marlab.ac.uk>
2015-03-12 17:01:03 +0000

    added loader function

--- R/utilities.R
+++ R/utilities.R
@@ -3,6 +3,22 @@
 NULL
 
 
+#' Progress function
+#'
+#' Simply displays a bar showing progress
+#'
+#' @param p Value
+#' @return NULL
+#' @export
+loader <- function(p)
+{
+  if (p==0) cat("0%                       50%                     100%\n")
+ str <- paste0(rep(c("\r[", "=", ">", " ", "]"), c(1, floor(p*50), 1, 50 - floor(p*50), 1)), collapse = "")  
+ cat(str); flush.console()
+ if (floor(p) == 1) cat("\n")
+}
+
+
 
 
 #' Run a full calibration
